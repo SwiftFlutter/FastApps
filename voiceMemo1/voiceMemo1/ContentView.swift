9dee1d734f49ab9562cf0a0eef6037d581a164fb
SwiftFlutter <ericshin215@gmail.com>
2024-06-13 20:40:29 +0900

    6/13

---
+++ voiceMemo1/voiceMemo1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  voiceMemo1
+//
+//  Created by JK on 6/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ voiceMemo1/voiceMemo1/voiceMemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemo1App.swift
+//  voiceMemo1
+//
+//  Created by JK on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
